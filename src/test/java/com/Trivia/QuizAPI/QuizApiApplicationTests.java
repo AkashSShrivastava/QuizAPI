4a3da3fbe31723585a40e1f222d2bb0b130f6c59
Akash Shekhar Shrivastava <sas5198sas@gmail.com>
2022-07-31 04:59:22 -0700

    Initial commit

---
+++ src/test/java/com/Trivia/QuizAPI/QuizApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Trivia.QuizAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
